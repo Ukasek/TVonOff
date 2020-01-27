f07b3731ba12b3886327743f5ddeab603596f487
Lukasz Szczupak <szczupak.lukasz@o2.pl>
2020-01-27 17:11:03 +0100

    TvOn poprawa

--- src/Televisor.java
+++ src/Televisor.java
@@ -1,15 +1,18 @@
 public class Televisor {
     boolean isOn;
 
-    Televisor(boolean isOn) {
-        isOn = false;
+    Televisor(boolean On) {
+        isOn = On;
     }
+
     void turnOn() {
         isOn = true;
     }
+
     void turnOff() {
         isOn = false;
     }
+
     void showStatus() {
         System.out.println("Telewizor jest włączony: " + isOn);
     }
